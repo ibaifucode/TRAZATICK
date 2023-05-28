6de8fee1f3442c57f8a661c6fdc3e890c4c10e42
Ibai <ibaifuentes@lksnext.com>
2023-05-28 14:45:26 +0200

    actu

--- src/app/countries/components/raee-filter-panel/raee-filter-panel.component.ts
+++ src/app/countries/components/raee-filter-panel/raee-filter-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Donde, Proveedor, Provincia, Raee, Region, TipoLectura, TipoRAEE } from '../../interfaces/raee.interface';
 
 @Component({
   selector: 'app-raee-filter-panel',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./raee-filter-panel.component.css']
 })
 export class RaeeFilterPanelComponent {
+  public raeeabuscar: Raee = {
+    CargaDatosLecturasId: 0, // Asigna el valor adecuado
+    FechaLectura: '',
+    CodigoEtiqueta: '',
+    GeoPosicion: '',
+    TipoLectura: TipoLectura.Entrada,
+    Donde: Donde.Cac,
+    Region: Region.Cantabria,
+    Provincia: Provincia.Cantabria,
+    Proveedor: Proveedor.CantabriaRecycling,
+    TipoRAEE: TipoRAEE.FR1Frigoríficos,
+    DescripcionResiduo: '',
+    Marca: '',
+    Modelo: '',
+    Peso: ''
+  };
+  public filtrolectura: boolean = false;
+  public filtroRAEE: boolean = false;
 
+  // Métodos para obtener las opciones válidas de cada casilla desplegable
+  getTipoLecturaOptions(): string[] {
+    return Object.values(TipoLectura);
+  }
+
+  getDondeOptions(): string[] {
+    return Object.values(Donde);
+  }
+
+  getRegionOptions(): string[] {
+    return Object.values(Region);
+  }
+
+  getProvinciaOptions(): string[] {
+    return Object.values(Provincia);
+  }
+
+  getProveedorOptions(): string[] {
+    return Object.values(Proveedor);
+  }
+
+  getTipoRAEEOptions(): string[] {
+    return Object.values(TipoRAEE);
+  }
 }
